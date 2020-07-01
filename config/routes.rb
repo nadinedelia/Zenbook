e39642454a1f58f993277442871c715684b98d83
minsi yang <yangminsi@gmail.com>
2020-07-01 15:46:26 +0100

    created users controller and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/login'
+
+  get 'sessions/welcome'
+
+  get 'users/new'
+
+  get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 
