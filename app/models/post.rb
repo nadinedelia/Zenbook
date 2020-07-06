c0565585f1e49fc99b089b51da9b899ea4faadb2
minsi yang <yangminsi@gmail.com>
2020-07-06 11:41:57 +0100

    newest item show up first

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     validates :message, presence: true
     # belongs_to :user
 
+    default_scope {order(created_at: :desc)}
+    # default setting order for created_at column, newest item first
 end
