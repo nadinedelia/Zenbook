219b3d8b4ed5b46db4b41461563beb2bfb679708
minsi yang <yangminsi@gmail.com>
2020-07-07 15:38:30 +0100

    the COMMENT table is WORKINGgit add .git add .

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,18 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    p @post
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    p @post.comments.first
     @comment.save
-    flash[:msg] = 'Your post was successful!'
-    redirect_to posts_path(@post)
+    p @post.comments.first
+    flash[:msg] = 'Your comment was successful!'
+    redirect_to posts_path
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
