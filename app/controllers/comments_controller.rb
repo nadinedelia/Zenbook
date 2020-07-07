a6874ab6718eeb2ecb8c55b7573af95dfa05ba08
minsi yang <yangminsi@gmail.com>
2020-07-07 17:02:03 +0100

    destroy link added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,26 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    p @post
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
-    p @post.comments.first
     @comment.save
-    p @post.comments.first
     flash[:msg] = 'Your comment was successful!'
     redirect_to posts_path
   end
+
+#   def edit
+#     @post = Post.find(params[:post_id])
+#     @comment = @post.comments.find(params[:id])
+#   end
+
+    def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+
+      @comment.destroy
+      flash[:msg] = "Comment gone!!"
+      redirect_to posts_path
+    end
 
   private
   def comment_params
