6a346aa1aa10347cc4c3187d115e14325d5c5c60
minsi yang <yangminsi@gmail.com>
2020-07-08 12:35:11 +0100

    completed edit, update action on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
@@ -8,22 +9,31 @@ class CommentsController < ApplicationController
     redirect_to posts_path
   end
 
-#   def edit
-#     @post = Post.find(params[:post_id])
-#     @comment = @post.comments.find(params[:id])
-#   end
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
 
-    def destroy
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.find(params[:id])
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.update(comment_params)
+    flash[:msg] = 'Your comment is updated!'
+    redirect_to posts_path
+  end
 
-      @comment.destroy
-      flash[:msg] = "Comment gone!!"
-      redirect_to posts_path
-    end
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    @comment.destroy
+    flash[:msg] = "Comment gone!!"
+    redirect_to posts_path
+  end
 
   private
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id)
+    params.require(:comment).permit(:content)
   end
+
 end
