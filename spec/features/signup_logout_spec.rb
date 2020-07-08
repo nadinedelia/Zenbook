0d42213df519b598ad56ae016391860e6fdad4dc
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-08 16:09:08 +0100

    Updating feature test. Comments section

---
+++ spec/features/signup_logout_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require 'web_helpers'
+
+feature 'signup page' do
+  scenario "user can sign up to Zenbook" do
+    signup
+    expect(page).to have_current_path('/')
+  end
+end
+  #may need to edit this test if we change root page for the site
+
+  feature 'logout' do
+    scenario "user can log out of Zenbook" do
+      signup
+      click_link 'Log Out'
+      expect(page).to have_current_path('/login')
+    end
+  end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,5 +1,5 @@
 def signup
-visit ('/signup')
+    visit ('/signup')
     fill_in 'user_name', with: 'Dave'
     fill_in 'email', with: 'dave@test.co.uk'
     fill_in 'password', with: 'TestPasW0rd'
