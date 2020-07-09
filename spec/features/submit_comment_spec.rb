ea32aaa2fd41c24e366fecfbd45dbc693ca23b56
nadinedelia <nadine.delia@yahoo.com>
2020-07-09 12:32:58 +0200

    comment feature test passing

--- spec/features/submit_comment_spec.rb
+++ spec/features/submit_comment_spec.rb
@@ -1,14 +1,16 @@
-# require 'rails_helper'
-# require 'web_helpers'
+require 'rails_helper'
+require 'web_helpers'
 
-# feature 'commenting on post' do
-#   scenario 'user can create comment on any post' do
-#     signup
-#     click_link "New post"
-#     fill_in "Message", with: "Delete me!"
-#     click_button "Share"
-#     fill_in "comment", with: 'amazing post!'
-#     click_button 'comment'
-#     expect(page).to have_content("amazing post!")
-#   end
-# end
+feature 'commenting on post' do
+  scenario 'user can create comment on any post' do
+    signup
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Delete me!"
+    click_button "Share"
+    # expect to stay on same page
+    find('.field', match: :first).set("amazing post!")
+    click_button "comment"
+    expect(page).to have_content("amazing post!")
+  end
+end
