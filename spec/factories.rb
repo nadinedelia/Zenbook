be2eea6be204a0dc5b2a2d7d824be67ee23c62c3
nadinedelia <nadine.delia@yahoo.com>
2020-07-09 13:25:03 +0200

    login test working

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,9 +2,18 @@ FactoryBot.define do
   # sequence :email do |n|
   #   "user#{n}@example.com"
   # end
+
+  # factory :user do
+  #   name {"John"}
+  #   password {"12345"}
+  #   email { "#{name}@example.com" }
+  # end
+
   factory :user do
-    name {"John"}
-    password {"12345"}
-    email { "#{name}@example.com" }
-  end
+    sequence(:email) { |n| "tester_#{n}@example.com" }
+    sequence(:name) { |n| "tester_#{n}" }
+    password { "pass123" }
+    password_confirmation { "pass123" }
+ end
+
 end
